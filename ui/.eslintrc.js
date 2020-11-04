881335e13a34f734849a089f5a97e54823f68b77
Dorota Gil <gildota@wp.pl>
2020-11-04 14:41:53 +0100

    Configure ESLint & Prettier for UI

---
+++ ui/.eslintrc.js
@@ -0,0 +1,34 @@
+module.exports = {
+  root: true,
+  overrides: [
+    {
+      files: ["*.ts"],
+      parserOptions: {
+        project: ["tsconfig.*?.json", "e2e/tsconfig.json"],
+        createDefaultProgram: true,
+      },
+      extends: [
+          "plugin:@angular-eslint/recommended", 
+          "airbnb-typescript/base",
+          "prettier/@typescript-eslint",
+          "plugin:prettier/recommended",
+      ],
+      rules: {
+        "import/prefer-default-export": "off",
+        "@typescript-eslint/lines-between-class-members": ["error", "always", { "exceptAfterSingleLine": true }],
+        "@typescript-eslint/no-explicit-any": "error"
+      },
+    },
+    {
+      files: ["*.component.html"],
+      extends: ["plugin:@angular-eslint/template/recommended"],
+      rules: {
+        "max-len": ["error", { code: 140 }],
+      },
+    },
+    {
+      files: ["*.component.ts"],
+      extends: ["plugin:@angular-eslint/template/process-inline-templates"],
+    },
+  ],
+};

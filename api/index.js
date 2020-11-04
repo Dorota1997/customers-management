fde8710c08a19d9f60cc5c64748b6356475d16ef
Dorota Gil <gildota@wp.pl>
2020-11-04 15:57:46 +0100

    Add API

---
+++ api/index.js
@@ -0,0 +1,83 @@
+const fs = require('fs');
+const bodyParser = require('body-parser');
+const jsonServer = require('json-server');
+const jwt = require('jsonwebtoken');
+
+const server = jsonServer.create();
+const router = jsonServer.router('./customers.json');
+const userdb = JSON.parse(fs.readFileSync('./users.json', 'UTF-8'));
+
+server.use(bodyParser.urlencoded({ extended: true }));
+server.use(bodyParser.json());
+server.use(jsonServer.defaults());
+
+const SECRET_KEY = '$(3dfgb^!fdsGJW)8zy%&dM1';
+
+const expiresIn = '5h';
+
+function createToken(payload) {
+  return jwt.sign(payload, SECRET_KEY, { expiresIn });
+}
+
+function verifyToken(token) {
+  return jwt.verify(token, SECRET_KEY, (err, decode) =>
+    decode !== undefined ? decode : err
+  );
+}
+
+function isAuthenticated({ login, password }) {
+  return (
+    userdb.users.findIndex(
+      (user) => user.login === login && user.password === password
+    ) !== -1
+  );
+}
+
+server.post('/auth/login', (req, res) => {
+  const { login, password } = req.body;
+  if (isAuthenticated({ login, password }) === false) {
+    const status = 401;
+    const message = 'Incorrect email or password';
+    res.status(status).json({ status, message });
+    return;
+  }
+  const user = userdb.users.find((u) => u.login === login);
+  const id = user.id;
+  const role = user.role;
+  const access_token = createToken({ id, login, role });
+  res.status(200).json({ access_token });
+});
+
+server.use(/^(?!\/auth).*$/, (req, res, next) => {
+  if (
+    req.headers.authorization === undefined ||
+    req.headers.authorization.split(' ')[0] !== 'Bearer'
+  ) {
+    const status = 401;
+    const message = 'Error in authorization format';
+    res.status(status).json({ status, message });
+    return;
+  }
+  try {
+    let verifyTokenResult;
+    verifyTokenResult = verifyToken(req.headers.authorization.split(' ')[1]);
+
+    if (verifyTokenResult instanceof Error) {
+      const status = 401;
+      const message = 'Access token not provided';
+      res.status(status).json({ status, message });
+      return;
+    }
+    next();
+  } catch (err) {
+    const status = 401;
+    const message = 'Error access_token is revoked';
+    res.status(status).json({ status, message });
+  }
+});
+
+server.use(router);
+
+server.listen(8000, () => {
+  console.log('Run Auth API Server');
+});

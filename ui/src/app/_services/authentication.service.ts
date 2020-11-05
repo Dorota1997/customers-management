68ab079e2324de0c21b96e4f8a4ce6b4a002aecc
Dorota Gil <gildota@wp.pl>
2020-11-05 20:03:40 +0100

    Add login method

--- ui/src/app/_services/authentication.service.ts
+++ ui/src/app/_services/authentication.service.ts
@@ -1,8 +1,24 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { map } from 'rxjs/operators';
+import { TokenStorageService } from './token-storage.service';
+import { Token } from '../_models/token.model';
+import { Login } from '../_models/login.model';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthenticationService {
-  constructor() {}
+  authenticationUrl = `${environment.api}auth/login`;
+
+  constructor(private http: HttpClient, private tokenStorageService: TokenStorageService) {}
+
+  login(authData: Login) {
+    return this.http.post(this.authenticationUrl, authData).pipe(
+      map((response: Token) => {
+        this.tokenStorageService.saveToken(response.access_token);
+      }),
+    );
+  }
 }

78b7a01efc73475e1bd97d0ff9ec9781d71add52
Dorota Gil <gildota@wp.pl>
2020-11-05 13:43:57 +0100

    Add router-outlet

--- ui/src/app/app.component.ts
+++ ui/src/app/app.component.ts
@@ -7,18 +7,4 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'customers-managment';
-  boolea;
-
-  ngOnInit() {
-    const x = 2;
-    this.boolea === 3;
-    if (this.boolea == 15) {
-      console.log('kapp');
-    }
-    for (let x = 0; x > 2; x++) {
-      this.boolea = true;
-    }
-  }
-
-  ffafa() {}
 }

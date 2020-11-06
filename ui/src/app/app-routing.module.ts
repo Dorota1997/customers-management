55268001ac640ec750824b005b2d340499149c0c
Dorota Gil <gildota@wp.pl>
2020-11-06 14:05:06 +0100

    Create customers path

--- ui/src/app/app-routing.module.ts
+++ ui/src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CustomersComponent } from './components/customers/customers.component';
 import { LoginComponent } from './components/login/login.component';
 
 const routes: Routes = [
   {
     path: '',
     component: LoginComponent,
+  },
+  {
+    path: 'customers',
+    component: CustomersComponent,
   },
 ];
 

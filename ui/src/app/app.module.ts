794b1b0b7b188db16fe3bb128b3c6da6c6c5f70d
Dorota Gil <gildota@wp.pl>
2020-11-11 21:18:25 +0100

    Update AppModule

--- ui/src/app/app.module.ts
+++ ui/src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { MatTableModule } from '@angular/material/table';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -8,13 +9,22 @@ import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { CustomersComponent } from './components/customers/customers.component';
-import { AppComponent } from './app.component';
-import { AppRoutingModule } from './app-routing.module';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { NgxMaskModule } from 'ngx-mask';
+import { MatSortModule } from '@angular/material/sort';
+import { MatSelectModule } from '@angular/material/select';
+import { JwtModule } from '@auth0/angular-jwt';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { FormComponent } from './components/form/form.component';
+import { authInterceptorProviders } from './_helpers/auth.interceptor';
 import { LoginComponent } from './components/login/login.component';
-
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CustomersComponent } from './components/customers/customers.component';
+import { AuthenticationService } from './_services/authentication.service';
+import { SearchComponent } from './components/search/search.component';
 @NgModule({
-  declarations: [AppComponent, LoginComponent, CustomersComponent],
+  declarations: [AppComponent, LoginComponent, CustomersComponent, FormComponent, SearchComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
@@ -27,8 +37,15 @@ import { LoginComponent } from './components/login/login.component';
     MatIconModule,
     ReactiveFormsModule,
     FormsModule,
+    MatTableModule,
+    MatPaginatorModule,
+    NgxMaskModule.forRoot(),
+    MatSortModule,
+    MatSelectModule,
+    JwtModule.forRoot({ config: {} }),
+    MatExpansionModule,
   ],
-  providers: [],
+  providers: [AuthenticationService, authInterceptorProviders],
   bootstrap: [AppComponent],
 })
 export class AppModule {}

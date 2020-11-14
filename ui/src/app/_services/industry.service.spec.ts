559bb5bfb7bc3d6c50199744c3793a8189c09822
Dorota Gil <gildota@wp.pl>
2020-11-14 23:55:17 +0100

    Testing Industry Service

--- ui/src/app/_services/industry.service.spec.ts
+++ ui/src/app/_services/industry.service.spec.ts
@@ -1,14 +1,51 @@
-import { TestBed, inject } from '@angular/core/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { TestBed } from '@angular/core/testing';
+import { environment } from 'src/environments/environment';
 import { IndustryService } from './industry.service';
 
 describe('Service: Industry', () => {
+  let httpMock: HttpTestingController;
+  let industryService: IndustryService;
+
   beforeEach(() => {
     TestBed.configureTestingModule({
       providers: [IndustryService],
+      imports: [HttpClientTestingModule],
     });
+
+    httpMock = TestBed.get(HttpTestingController);
+    industryService = TestBed.get(IndustryService);
   });
 
-  it('should ...', inject([IndustryService], (service: IndustryService) => {
-    expect(service).toBeTruthy();
-  }));
+  afterEach(() => {
+    httpMock.verify();
+  });
+
+  it('should use GET method', () => {
+    industryService.industries().subscribe();
+    const request = httpMock.expectOne(`${environment.api}industries`);
+    expect(request.request.method).toBe('GET');
+  });
+
+  it('should use the right url', () => {
+    industryService.industries().subscribe();
+    const request = httpMock.expectOne(`${environment.api}industries`);
+    expect(request.request.url).toBe(`${environment.api}industries`);
+  });
+
+  it('should return the right data', () => {
+    const industriesArray = [
+      {
+        name: 'Finanse',
+        subcategories: ['Bank', 'Ubezpieczalnia'],
+      },
+    ];
+
+    industryService.industries().subscribe((industries) => {
+      expect(industries).toEqual(industriesArray);
+    });
+
+    const request = httpMock.expectOne(`${environment.api}industries`);
+    request.flush(industriesArray);
+  });
 });

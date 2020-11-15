65d3e8b2479c56e2df8d43669eee3b1cd0ee1ec2
Dorota Gil <gildota@wp.pl>
2020-11-15 19:38:04 +0100

    Testing Authentication Service

--- ui/src/app/_services/authentication.service.spec.ts
+++ ui/src/app/_services/authentication.service.spec.ts
@@ -1,14 +1,36 @@
-import { TestBed, inject } from '@angular/core/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { environment } from 'src/environments/environment';
+import { inject, TestBed } from '@angular/core/testing';
 import { AuthenticationService } from './authentication.service';
+import { LoginComponent } from '../components/login/login.component';
 
 describe('Service: Authentication', () => {
+  let httpMock: HttpTestingController;
+
   beforeEach(() => {
     TestBed.configureTestingModule({
+      declarations: [LoginComponent],
       providers: [AuthenticationService],
+      imports: [HttpClientTestingModule],
     });
+
+    httpMock = TestBed.get(HttpTestingController);
   });
 
-  it('should ...', inject([AuthenticationService], (service: AuthenticationService) => {
-    expect(service).toBeTruthy();
-  }));
+  afterEach(() => {
+    httpMock.verify();
+  });
+
+  it('should login correctly', inject(
+    [AuthenticationService],
+    (authService: AuthenticationService) => {
+      const user = {
+        login: 'admin',
+        password: 'k5o#v0qG@I',
+      };
+      authService.login(user).subscribe(() => {});
+      const request = httpMock.expectOne(`${environment.api}auth/login`);
+      expect(request.request.method).toBe('POST');
+    },
+  ));
 });
